48228082ad5c245b03397433c47d3283b4ecfb57
legionlabs-test-dev <himanshu333r@gmail.com>
2026-01-12 13:08:27 +0530

    added vscode

---
+++ files/scripts/install-vscode.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+sudo dnf install code -y
